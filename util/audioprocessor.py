cba15a629824cf7b34b5c879ef11cab2725aabd7
Antoine Mairesse <antoine.mairesse@free.fr>
2016-08-11 00:01:38 +0200

    Get the correct size for feat_vec, we should not try to learn on the padding data Exclude input data when we truncate the text but not the audio because data is probably inconsistent (TODO : add sanity checks between values of max_input_seq_length and max_target_seq_length to prevent bad ini values) Remove unused code

--- util/audioprocessor.py
+++ util/audioprocessor.py
@@ -17,50 +17,25 @@ class AudioProcessor(object):
         self.master_file_list = "master_file_list.txt"
         self.max_input_seq_length = max_input_seq_length
 
-    def run(self, file_name_text_pairs, output_dir, thread_num):
-        with open(os.path.join(output_dir, self.master_file_list), "a+") as f:
-            counter = 0
-            for file_name in file_name_text_pairs[0]:
-                counter += 1
-                if counter % 100 == 0:
-                    print("Processing file {0}... on thread {1}".format(counter,
-                                                                        thread_num))
-                # TODO : check following code because self.max_target_seq_length does not exist...
-                if len(file_name[1]) > self.max_target_seq_length:
-                    continue
-                wav_file = self.convertFlac2Wav(file_name[0])
-                feat_vec = self.computeLogMelFilterBank(wav_file)
-                if len(feat_vec) > self.max_input_seq_length:
-                    print("skipping")
-                    continue
-                elif len(feat_vec) < self.max_input_seq_length:
-                    pad_length = self.max_input_seq_length - len(feat_vec)
-                    padding = np.zeros((pad_length, 123), dtype=np.float)
-                    feat_vec = np.concatenate((feat_vec, padding), 0)
-                    assert len(feat_vec) == self.max_input_seq_length, "Padding incorrect..."
-                self.deleteWav(wav_file)
-                # save feature vector
-                array_file_name = os.path.basename(file_name[0]).replace(".flac", ".npy")
-                np.save(os.path.join(output_dir, array_file_name), feat_vec)
-                f.write("{0}, {1}\n".format(array_file_name,
-                    file_name[1]))
-
     def processFLACAudio(self, wav_file_name):
         '''
         Reads in audio file, processes it
         Returns padded feature tensor and unpadded length
         '''
         feat_vec = self.computeLogMelFilterBank(wav_file_name)
-        feat_vec_length = len(feat_vec)
-        if feat_vec_length > self.max_input_seq_length:
+        original_feat_vec_length = len(feat_vec)
+
+        if original_feat_vec_length > self.max_input_seq_length:
+            # Audio sequence too long, need to cut
             feat_vec = feat_vec[:self.max_input_seq_length]
-            feat_vec_length = len(feat_vec)
-        elif feat_vec_length <= self.max_input_seq_length:
-            pad_length = self.max_input_seq_length - len(feat_vec)
+        elif original_feat_vec_length < self.max_input_seq_length:
+            # Audio sequence too short, need padding to align each sequence
+            # (for technical reason, padded part won't be trained or tested)
+            pad_length = self.max_input_seq_length - original_feat_vec_length
             padding = np.zeros((pad_length, 123), dtype=np.float)
             feat_vec = np.concatenate((feat_vec, padding), 0)
         assert len(feat_vec) == self.max_input_seq_length, "Padding incorrect..."
-        return feat_vec, feat_vec_length
+        return feat_vec, original_feat_vec_length
 
     def convertAndDeleteFLAC(self, audio_file_name):
         wav_file = self.convertFlac2Wav(audio_file_name)
