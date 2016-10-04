d6dd65651fc573c4648ce7df3057e06ff4834d3d
Dominik <domkaukinen@gmail.com>
2016-10-03 23:52:42 -0400

    changed format of corpus from list to raw text

--- util/setuptextcorpus.py
+++ util/setuptextcorpus.py
@@ -4,4 +4,7 @@ except ImportError:
     print("nltk not installed, use 'pip install nltk'")
 
 def get_corpus_text():
-    text = [reuters.words(fid) for fid in reuters.fileids()]
+    '''
+    return raw text of reuters corpus
+    '''
+    return [" ".join(reuters.words(fid)) for fid in reuters.fileids()]
